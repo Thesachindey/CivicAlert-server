abaef063c4a4f329bb12c1344108f8cac4e281db
Sachin <sachinscode24@gmail.com>
2025-12-14 09:31:43 +0600

    added civicAlert clint side

---
+++ index.js
@@ -0,0 +1,62 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+require("dotenv").config()
+const app = express()
+const port = 3000
+// -------------------
+app.use(express.json())
+app.use(cors())
+//--------------
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.6fqewb1.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // --------------- start from here----------------
+        //database connection
+        const db = client.db('issue-db')
+    
+
+        //-------------------
+        //database theke data niye asbo, data manipulate korbo, data add korbo, by api methods
+
+        //GET all events data [find().toArray(), findOne().toArray()] by using GET api methods
+        //we can check GET api data by browser directly
+     
+
+
+
+
+
+
+        //----------------
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// --------------
+
+app.get('/', (req, res) => {
+    res.send('The CivicAlert data base running well!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
